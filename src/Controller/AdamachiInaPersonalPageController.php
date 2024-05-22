0aea73d0be52f218109607632867aaa1a7801792
Ina Adamachi <160891140+InaAdam@users.noreply.github.com>
2024-05-22 22:36:12 +0300

    added database

--- src/Controller/AdamachiInaPersonalPageController.php
+++ src/Controller/AdamachiInaPersonalPageController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Info;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdamachiInaPersonalPageController extends AbstractController
 {
+
     #[Route('/adamachi_ina', name: 'adamachi_ina_personal_page')]
     public function index(): Response
     {
@@ -15,4 +18,18 @@ class AdamachiInaPersonalPageController extends AbstractController
             'controller_name' => 'AdamachiInaPersonalPageController',
         ]);
     }
+
+
+    #[Route('/adamachi_database', name: 'database')]
+    public function home(EntityManagerInterface $elem): Response
+    {
+        $rep = $elem->getRepository(Info::class);
+        $inf = $rep->findAll();
+        dd($inf);
+
+        return $this->render('adamachi_ina_personal_page/adamachi_ina.html.twig');#, [
+        #    'controller_name' => 'AdamachiInaPersonalPageController',
+        #]);
+    }
+
 }
---
+++ src/Entity/Info.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InfoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InfoRepository::class)]
+class Info
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $hobby = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone_number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $occupation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getHobby(): ?string
+    {
+        return $this->hobby;
+    }
+
+    public function setHobby(string $hobby): static
+    {
+        $this->hobby = $hobby;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): static
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getOccupation(): ?string
+    {
+        return $this->occupation;
+    }
+
+    public function setOccupation(string $occupation): static
+    {
+        $this->occupation = $occupation;
+
+        return $this;
+    }
+}
